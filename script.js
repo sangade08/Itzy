ff3b44591d628fbc6270f64832792ba0c11e26d6
sangade08 <sangmadedivawikananda08@gmail.com>
2024-01-03 20:52:58 +0800

    meribah scipt di js menjadi selamat ulangtahun

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log('hello world')
+/**
+ * Function to say "Happy Birthday".
+ *
+ * @param {string} name - The name of the person whose birthday it is.
+ * @returns {string} A birthday message for the person.
+ */
+function sayHappyBirthday(name) {
+    return `Happy Birthday, ${name}!`;
+}
+ 
+// Usage Example
+const birthdayMessage = sayHappyBirthday("John");
+console.log(birthdayMessage);  // Output: Happy Birthday, John!
